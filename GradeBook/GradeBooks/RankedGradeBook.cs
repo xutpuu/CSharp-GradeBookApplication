5b1a90ea83da06120fce3c3a3399a6b961670b3a
xutpuu <xutpuu@gmail.com>
2018-09-24 16:47:37 +0300

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -33,14 +33,14 @@ namespace GradeBook.GradeBooks
         public override void CalculateStatistics()
         {
             if (Students.Count < 5)
-                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+               Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
             base.CalculateStatistics();
         }
 
         public override void CalculateStudentStatistics(string name)
         {
             if (Students.Count < 5)
-                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
             base.CalculateStudentStatistics(name);
         }
     }
